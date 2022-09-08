0e417bdda91912eb6a295e73af8ad97bfd43aeb4
mithra <studyuse06@gmail.com>
2022-09-08 20:00:42 +1000

    this is the commit for web application

--- app.py
+++ app.py
@@ -7,27 +7,28 @@ model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
     '''
     For rendering results on HTML GUI
     '''
+    
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+    return render_template('home.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     '''
     For direct API calls trought request
     '''
-    data = request.get_json(force=True)
+    data = request.json['data']
     prediction = model.predict([np.array(list(data.values()))])
 
     output = prediction[0]
--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
+from sklearn.linear_model import LinearRegression
 
 dataset = pd.read_csv('hiring.csv')
 
@@ -25,7 +26,7 @@ y = dataset.iloc[:, -1]
 #Splitting Training and Test Set
 #Since we have a very small dataset, we will train our model with all availabe data.
 
-from sklearn.linear_model import LinearRegression
+
 regressor = LinearRegression()
 
 #Fitting model with trainig data
